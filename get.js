12c45a5a5c177cda3756251dd7bb0f7793560307
danielkarlsson2 <daniel@dkmusic.se>
2022-11-10 16:04:37 +0100

    godkänt klar

--- get.js
+++ get.js
@@ -3,6 +3,9 @@ const output = document.querySelector('#tasks');
 // const textOutput =  document.querySelector('#textOutput');
 // const emailOutput =  document.querySelector('#emailOutput');
 
+
+      
+
 const getTasks = async () => {
     const res = await fetch("https://webapi-uppgift1.azurewebsites.net/api/customerTasks")
     const data = await res.json()
@@ -16,31 +19,43 @@ const getTasks = async () => {
         }
         return 0;
     }
-    data.sort(compare);
+    data.sort(compare); 
 
     console.log(data)
 
+
     for(let item of data) {
+
 
         const date = new Date();
         // date = item.CreatedDate
         
         output.innerHTML += `
-        <div id="${item.Id}" class=" border rounded bg-white p-2 mb-2 d-flex justify-content-between mt-3">
+        <div id="${item.Id}" class=" border rounded bg-white p-2 mb-2 d-flex justify-content-between mt-3 taskCard">
 
             <div class="d-flex flex-column p-2 w-50 ">
-                    <h2 class="">${item.Headline}</h2>
-                    <p class="">- ${item.TextMessage}</p>
+
+                    <h2 class="head">${item.Headline}</h2>
+                    <p class="mb-4 mt-3">- ${item.TextMessage}</p>
+
+                    <span class="span mt-4">User</span>
                     <p class="">${item.Email}</p>
                 
-                    <button class="btn btn-success w-50">Comment</button>
+                   
                 </div>
                 
                 <div class="d-flex flex-column justify-content-between p-2">
-                    <p class="mt-5  ">${item.CreatedDate}</p>
-                    <br>    
-                    <span class="span">Status:</span>
-                    <button class="btn mt-auto ${item.TaskStatus == true ? "Done" : 'notDone'} ">${item.TaskStatus === true ? "Done" : "Not done"}</button>
+                    <div>
+                        <span class="mt-5 span">Date and time:</span>
+                        <p class="">${item.CreatedDate}</p>
+                    </div>
+
+                    <div class="d-flex flex-column">  
+                        <span class="span">Status:</span>
+                        <button class="btn mt-auto comment btnSm ${item.TaskStatus == true ? "Done" : 'notDone'}" onclick="${item.TaskStatus !== item.TaskStatus}">
+                        ${item.TaskStatus === true ? "Done" : "Not done"}
+                        </button>
+                    </div>
                 </div>
             </div>
             `
@@ -51,3 +66,4 @@ const getTasks = async () => {
 
 getTasks();
 
+{/* <button class="btn btn-success w-50 comment">Add Comment</button> */}
