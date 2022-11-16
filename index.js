9946e51d3ae4aeffba443e4ccc13640d1c911991
danielkarlsson2 <daniel@dkmusic.se>
2022-11-16 13:58:06 +0100

    Godkänd klar

--- index.js
+++ index.js
@@ -41,35 +41,19 @@ const validateEmail = email => {
     }
 }
 
-    // taskStatus.addEventListener("change", () => {
-    //     if (taskStatus.checked) {
-    //         taskStatus.value = 1
-    //         textStatus.innerHTML = "Finished"
-
-    //     }
-    //     else {
-    //         taskStatus.value = 0
-    //         textStatus.innerHTML = ""
-    //     }
-    // })
-
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    // for (let element of e.target)
-    //     console.log(element.value)
+
 
     let customerTask = {
         headline: e.target[0].value,
         email: e.target[1].value,
         createdDate: e.target[2].value,
         textMessage: e.target[3].value,
-        taskStatus: e.target[4].value
+        taskStatus: Number(e.target[4].value)
     }
-    console.log(customerTask)
-    
-  
+    console.log(customerTask)  
 
     fetch("https://webapi-uppgift1.azurewebsites.net/api/customerTasks", {
         method: 'post',
@@ -81,43 +65,11 @@ form.addEventListener('submit', (e) => {
 
     })
 
+    headline.value = '';
+    email.value ='';
+    createdDate.value = '';
+    textMessage.value = '';
+    taskStatus.value = 0
+
 
 })
-
-// function getTasks(task) {
-//     const tasks = {
-//         headline,
-//         email, 
-//         createdDate,
-//         textMessage,
-//         taskStatus
-//     };
-//     taskItems.push(task);
-//     console.log(taskItems);
-
-//     fetch("https://webapi-uppgift1.azurewebsites.net/api/customerTasks", {
-//         method: 'get',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(customerTask)
-
-
-//     })
-// }
-
-// const tasks = document.querySelector('#tasks');
-// const listTasks = () => {
-//     // output.innerHTML = '';
-//     tasks.forEach(task => {
-//         output.innerHTML += `
-//         <div id="${task.id}" class="d-flex justify-content-between align-items-center border bg-white p-2 mb-2">
-//                 <h3 class="text-center p-4">${task.headline}</h3>
-//                 <p class="m-0 h4">${task.textMessage}</p>
-//                 <button type ="button" class="btn btn-danger btn-sm">X</button>
-//             </div>
-//             `
-//     })
-// }
-
-// listTasks();
